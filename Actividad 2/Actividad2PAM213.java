4476b82aec7ff0316434e425095182db3a456abe
Anapaulara <124048816@upq.edu.mx>
2025-09-04 22:00:19 -0600

    Se inicio un código en java.

---
+++ Actividad 2/Actividad2PAM213.java
@@ -0,0 +1,47 @@
+public class Actividad2PAM213 {
+    
+    public String ReglamentoPOO() {
+        return "Reglamento POO:\n"
+             + "1. Se requiere 80% de asistencia para tener derecho a evaluación parcial y 80% de trabajos en clase.\n"
+             + "2. Se permiten 10 minutos de tolerancia y si el alumno llega después de este tiempo puede permanecer en la clase, pero no se tomará la asistencia (Solamente en los horarios de inicio:7:00a.m y 14:00 p.m).\n"
+             + "3. Las faltas deberán estar justificadas mediante el correo institucional con un plazo máximo de 24 horas posteriores a la hora de falta en clase mediante correo del tutor (a), justificantes entregados fuera de la fecha límite permitido no se aceptan, únicamente se aceptarán recetas médicas y citatorios jurídicos.\n"
+             + "4. Las tareas y trabajos deberán subirlas al Classroom de forma individual y no se recibirán de manera extemporánea.\n"
+             + "5. Las tareas y trabajos presentarlos en tiempo y forma. La demora de un trabajo o tarea sin justificante y/o con justificante fuera del límite no se aceptan.\n"
+             + "6. Utilizar el correo institucional para trabajar bajo la plataforma Google Classroom.\n"
+             + "7. El plagio o copia de trabajos y/o exámenes será condicionado a reprobar la asignatura y se reportará al área correspondiente.\n"
+             + "8. Cualquier deshonestidad académica será sancionada reprobando el parcial sin derecho a examen final.\n"
+             + "9. Indisciplina o falta de respeto: primera llamada de atención; reincidencia = abandonar el aula; 3 incidencias = sin derecho a examen.\n"
+             + "10. Uso de laptops y/o dispositivos móviles limitado a actividades académicas.\n"
+             + "11. Prohibido el uso de audífonos durante la clase.\n"
+             + "12. Prohibido comer y/o tomar líquidos en el salón.\n"
+             + "13. Prohibido sentarse encima de las mesas o columpiarse en las sillas.\n"
+             + "14. Todo tema académico debe seguir l canal: docente → tutor → coordinación → dirección.\n"
+             + "15. Situaciones no previstas: dirección del programa educativo.\n"
+             + "16. Día de entrega de calificaciones: asistencia obligatoria.\n"
+             + "17. Este reglamento entra en vigor al ser aceptado por la mayoría en la primera sesión.\n";
+    }
+    e
+    public String LineamientosClassroom() {
+        return "Lineamientos Classroom:\n"
+             + "1. Participación.\n"
+             + "2. Trabajos en clase.\n"
+             + "3. Entrega completa.\n"
+             + "4. Respeto en clase.\n"
+             + "5. Entregar trabajos de calidad.\n";
+    }
+    
+    public String FechasdeParciales() {
+        return "Fechas de Parciales:\n"
+             + "1er Parcial: 30-09-25\n"
+             + "2do Parcial: 04-10-25\n"
+             + "3er Parcial: 02-12-25\n";
+    }
+    
+    public String PorcentajesporParcial() {
+        return "Porcentajes por Parcial:\n"
+             + "Evidencia de Conocimiento:  1er parcial 40% | 2do parcial 40% | 3er parcial 20%\n"
+             + "Evidencia de Desempeño:     1er parcial 20% | 2do parcial 20% | 3er parcial 10%\n"
+             + "Evidencia de Producto:      1er parcial 30% | 2do parcial 20% | 3er parcial 40%\n"
+             + "Proyecto Integrador:        1er parcial 10% | 2do parcial 20% | 3er parcial 30%\n";
+    }
+}
