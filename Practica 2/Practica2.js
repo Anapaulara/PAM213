706650944949fb3de1a4638c909635dd8a6fe7da
Anapaulara <124048816@upq.edu.mx>
2025-09-23 23:01:44 -0600

    Ejercicio B Objetos y Arreglos.

--- Practica 2/Practica2.js
+++ Practica 2/Practica2.js
@@ -12,4 +12,20 @@ const persona = {
 const {nombre, edad, direccion: {ciudad} } = persona; 
 
 //Imprime mensaje aqui 
-console.log ("Me llamo " + nombre + ", tengo " + edad + " años"+ " y vivo en "+ ciudad); 
+console.log ("Me llamo " + nombre + ", tengo " + edad + " años"+ " y vivo en "+ ciudad);
+
+
+const productos = [
+  { nombre: "Laptop", precio: 12000 },
+  { nombre: "Mouse", precio: 250 },
+  { nombre: "Teclado", precio: 750 },
+  { nombre: "Monitor", precio: 3000 }
+];
+//Codigo
+//Productos con precio > 1000
+const filtrados = productos.filter(p => p.precio > 1000);
+
+//Arreglo con solo nombres
+const nombres = filtrados.map(p => p.nombre);
+
+console.log(nombres); // ["Laptop", "Monitor"]
